b7f2092b214450d231aafe2b440a5be102790011
wk <somnuk.wk@gmail.com>
2017-08-25 14:40:23 +0700

    Test query

--- script.clj
+++ script.clj
@@ -1,1 +1,13 @@
-lein repl :connect 192.168.0.20:7888
+lein repl :connect 7888
+(require '[alfresco.auth :as a])
+(require '[alfresco.search :as s])
+(require '[alfresco.nodes :as n])
+
+(def company-home 
+    (a/as-admin
+     (first
+       (s/query "PATH:\"/*\" AND TYPE:\"cm:folder\""))))
+        
+(clojure.pprint/pprint
+  (a/as-admin
+    (n/properties company-home)))
