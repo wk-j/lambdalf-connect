02d242655c855325c2d5e8dcd01ece1ccdb92580
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-25 15:38:10 +0700

    Print property value

--- script.clj
+++ script.clj
@@ -1,4 +1,14 @@
+
+;; start nrepol
+curl -u admin:admin -X POST http://192.168.0.20:8080/alfresco/service/clojure/nrepl
+
+;; stop nrepol
+curl -u admin:admin -X DELETE http://192.168.0.20:8080/alfresco/service/clojure/nrepl
+
+;; connect
+lein repl :connect 192.168.0.20:7888
 lein repl :connect 7888
+    
 (require '[alfresco.auth :as a])
 (require '[alfresco.search :as s])
 (require '[alfresco.nodes :as n])
@@ -8,6 +18,17 @@ lein repl :connect 7888
      (first
        (s/query "PATH:\"/*\" AND TYPE:\"cm:folder\""))))
         
+
+
+(str company-home)
+
+(a/as-admin
+  (do
+    (println (n/site? company-home))
+    (println (n/dir? company-home))
+    (println (n/type-qname company-home))))
+
+
 (clojure.pprint/pprint
   (a/as-admin
     (n/properties company-home)))
